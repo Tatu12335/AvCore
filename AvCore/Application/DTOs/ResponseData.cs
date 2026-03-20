15913e33ec087e1b9f5eb4f7d4982d4e35df352a
tatus <tatusolavi@gmail.com>
2026-03-20 23:41:05 +0200

    Add project files.

---
+++ AvCore/Application/DTOs/ResponseData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace AvCore.Application.DTOs
+{
+    public class Response
+    {
+        // query-status ,data , signature and filename.
+        [JsonPropertyName("query-status")] public string QueryStatus { get; set; }
+        [JsonPropertyName("data")] public List<MalwareData> Data { get; set; }
+
+    }
+}
